869b1fb9ff361d548f95e8f126126d00d1d02b53
pee47kay <papakowdadson@gmail.com>
2022-09-06 10:10:35 +0000

    trying merge

--- src/shared/repositoriesCard.jsx
+++ src/shared/repositoriesCard.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import {
-  FaRegEye,
-  FaRegEyeSlash,
   FaExclamation,
-  FaUserEdit,
   FaUserClock,
   FaUsers,
   FaCodeBranch,
@@ -15,14 +12,31 @@ function RepositoriesCard({ repo }) {
     <div className="repositoryCard">
       <h4>{repo.name}</h4>
       <p className="description">{repo.description}</p>
-      <a target="_blanck" rel="noreferrer" href={`${repo.html_url}`} className="repoUrl">
+      <a
+        target="_blanck"
+        rel="noreferrer"
+        href={`${repo.html_url}`}
+        className="repoUrl"
+      >
         Visit Repository
       </a>
-      <div className="repoCardIcons" >
-        <div className="iconsPaddingLeft" ><FaUserClock />{repo.created_at}</div>
-        <div className="iconsPaddingLeft"><FaCodeBranch />{repo.forks_count}</div>
-        <div className="iconsPaddingLeft"><FaUsers />{repo.watchers}</div>
-        <div ><FaExclamation/>{repo.open_issues_count}</div>
+      <div className="repoCardIcons">
+        <div className="iconsPaddingLeft">
+          <FaUserClock />
+          {repo.created_at}
+        </div>
+        <div className="iconsPaddingLeft">
+          <FaCodeBranch />
+          {repo.forks_count}
+        </div>
+        <div className="iconsPaddingLeft">
+          <FaUsers />
+          {repo.watchers}
+        </div>
+        <div>
+          <FaExclamation />
+          {repo.open_issues_count}
+        </div>
       </div>
     </div>
   );
